3509eb951b38a1848e8d720392c3d71364880110
jathu1972-hub <jathu1972@gmail.com>
2026-04-19 12:58:02 +0530

    Update cookies.js

--- server/utils/cookies.js
+++ server/utils/cookies.js
@@ -2,27 +2,23 @@ import { env } from "../config/env.js";
 
 function durationToMilliseconds(duration) {
   const match = /^(\d+)([smhd])$/i.exec(duration);
-
   if (!match) {
     return 12 * 60 * 60 * 1000;
   }
-
   const value = Number(match[1]);
   const unit = match[2].toLowerCase();
-
   const multipliers = {
     s: 1000,
     m: 60 * 1000,
     h: 60 * 60 * 1000,
     d: 24 * 60 * 60 * 1000
   };
-
   return value * multipliers[unit];
 }
 
 const baseCookieOptions = {
-  sameSite: "strict",
-  secure: env.isProduction,
+  sameSite: "none",
+  secure: true,
   path: "/"
 };
 
