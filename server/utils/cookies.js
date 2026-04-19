0f29143795bc618e175fa337f1bae408c53068d1
jathu1972-hub <jathu1972@gmail.com>
2026-04-19 13:11:49 +0530

    Update cookies.js

--- server/utils/cookies.js
+++ server/utils/cookies.js
@@ -2,43 +2,36 @@ import { env } from "../config/env.js";
 
 function durationToMilliseconds(duration) {
   const match = /^(\d+)([smhd])$/i.exec(duration);
-  if (!match) {
-    return 12 * 60 * 60 * 1000;
-  }
+  if (!match) return 12 * 60 * 60 * 1000;
   const value = Number(match[1]);
   const unit = match[2].toLowerCase();
-  const multipliers = {
-    s: 1000,
-    m: 60 * 1000,
-    h: 60 * 60 * 1000,
-    d: 24 * 60 * 60 * 1000
-  };
+  const multipliers = { s: 1000, m: 60 * 1000, h: 60 * 60 * 1000, d: 24 * 60 * 60 * 1000 };
   return value * multipliers[unit];
 }
 
 const baseCookieOptions = {
   sameSite: "none",
   secure: true,
-  path: "/"
+  path: "/",
 };
 
 export const csrfCookieOptions = {
   ...baseCookieOptions,
   httpOnly: false,
-  maxAge: durationToMilliseconds(env.jwtExpiresIn)
+  maxAge: durationToMilliseconds(env.jwtExpiresIn),
 };
 
 export function setAuthCookie(res, token) {
   res.cookie("auth_token", token, {
     ...baseCookieOptions,
     httpOnly: true,
-    maxAge: durationToMilliseconds(env.jwtExpiresIn)
+    maxAge: durationToMilliseconds(env.jwtExpiresIn),
   });
 }
 
 export function clearAuthCookie(res) {
   res.clearCookie("auth_token", {
     ...baseCookieOptions,
-    httpOnly: true
+    httpOnly: true,
   });
 }
